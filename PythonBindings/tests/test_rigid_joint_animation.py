Fix joint constaint computation; Add init status for bias

PythonBindings/tests/test_rigid_joint_animation.py
@@ -34,38 +34,38 @@ cube_mesh = trimesh.load(cube_mesh_path, process=False)
 
 
 # Body A: kinematic driver (fixed all vertices) to provide a moving target.
-def make_driver_body():
+def make_driver_body(offset: float = 0.0):
     body = solver.create_world_data_from_array("driver", cube_mesh.vertices, cube_mesh.faces)
     body.set_simulation_type(lcs.MaterialType.Rigid)
     body.set_scale(0.10)
-    body.set_translation(0.2, 0.20, 0.0)
+    body.set_translation(0.2, 0.20, 0.0 + offset)
     # body.add_fixed_point_by_method("All")
     return solver.register_world_data(body)
 
 
 # Body B: dynamic rigid body constrained by joint.
-def make_follower_body():
+def make_follower_body(offset: float = 0.0):
     body = solver.create_world_data_from_array("follower", cube_mesh.vertices, cube_mesh.faces)
     body.set_simulation_type(lcs.MaterialType.Rigid)
     body.set_scale(0.10)
-    body.set_translation(0.0, 0.2, 0.0)
+    body.set_translation(0.0, 0.2, 0.0+ offset)
     return solver.register_world_data(body)
 
 # Body C
-def make_obstacle_body():
+def make_obstacle_body(offset: float = 0.0):
     body = solver.create_world_data_from_array("Obstacle", cube_mesh.vertices, cube_mesh.faces)
     body.set_simulation_type(lcs.MaterialType.Rigid)
     body.set_scale(0.10)
-    body.set_translation(-0.05, 0.01, 0.0)
+    body.set_translation(-0.05, 0.01, 0.0+ offset)
     body.add_fixed_point_by_method("All")
     return solver.register_world_data(body)
 
 
-def add_joint(driver_id: int, follower_id: int):
+def add_joint(driver_id: int, follower_id: int, joint: str):
     a0 = np.array([0.0, 0.0, 0.0], dtype=np.float32)
     b0 = np.array([0.0, 0.0, 0.0], dtype=np.float32)
 
-    if args.joint == "fixed":
+    if joint == "fixed":
         solver.add_fixed_joint(
             driver_id,
             follower_id,
@@ -74,7 +74,7 @@ def add_joint(driver_id: int, follower_id: int):
             stiffness_pos=2.0e4,
             stiffness_rot=4.0e3,
         )
-    elif args.joint == "prismatic":
+    elif joint == "prismatic":
         solver.add_prismatic_joint(
             driver_id,
             follower_id,
@@ -84,7 +84,7 @@ def add_joint(driver_id: int, follower_id: int):
             stiffness_pos=2.0e4,
             stiffness_rot=4.0e3,
         )
-    else:
+    elif joint == "revolute":
         solver.add_revolute_joint(
             driver_id,
             follower_id,
@@ -97,14 +97,28 @@ def add_joint(driver_id: int, follower_id: int):
             stiffness_axis=2.0e3,
         )
 
+fixed_joint_offset = 0.0
+fixed_joint_a = make_driver_body(fixed_joint_offset)
+fixed_joint_b = make_follower_body(fixed_joint_offset)
+add_joint(fixed_joint_a, fixed_joint_b, "fixed")
+fixed_joint_c = make_obstacle_body(fixed_joint_offset)
 
-driver_id = make_driver_body()
-follower_id = make_follower_body()
-add_joint(driver_id, follower_id)
-make_obstacle_body()
+prismatic_joint_offset = 0.2
+prismatic_joint_a = make_driver_body(prismatic_joint_offset)
+prismatic_joint_b = make_follower_body(prismatic_joint_offset)
+add_joint(prismatic_joint_a, prismatic_joint_b, "prismatic")
+prismatic_joint_c = make_obstacle_body(prismatic_joint_offset)
+
+revolute_joint_offset = 0.4
+revolute_joint_a = make_driver_body(revolute_joint_offset)
+revolute_joint_b = make_follower_body(revolute_joint_offset)
+add_joint(revolute_joint_a, revolute_joint_b, "revolute")
+revolute_joint_c = make_obstacle_body(revolute_joint_offset)
+
 
 solver.init_solver()
 config_ref = solver.get_config()
+config_ref.use_floor = True
 output_dir = os.path.join(root, "Resources", "OutputMesh")
 os.makedirs(output_dir, exist_ok=True)
 